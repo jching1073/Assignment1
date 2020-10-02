6091f905e2f884c0ae9d7922b06b099d3715ce65
Jerome Ching <nathan_9_9@hotmail.com>
2020-10-02 11:10:34 -0700

    Lecture5Movements

---
+++ Assets/Scipts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //private variables
+
+    private Rigidbody2D rbody;
+    private bool isGrounded = false;
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float jumpForce = 500.0f;
+    [SerializeField] private float groundCheckRadius = 0.15f;
+    [SerializeField] private Transform groundGcheckPos;
+    [SerializeField] private LayerMask whatIsGround;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+  
+    //physics
+    private void FixedUpdate()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        isGrounded = GroundCheck();
+
+        // jump code
+
+        if (isGrounded && Input.GetAxis("Jump") > 0)
+        {
+            rbody.AddForce(new Vector2(0.0f, jumpForce));
+            isGrounded = false;
+        }
+        rbody.velocity = new Vector2(horiz * speed, rbody.velocity.y);
+    }
+
+    private bool GroundCheck()
+    {
+        return Physics2D.OverlapCircle(groundGcheckPos.position, groundCheckRadius, whatIsGround);
+        
+    }
+}
